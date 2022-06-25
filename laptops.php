b84a5cce20eeb83e4ffc427b044e1f19e08ee391
Denismugabe1000 <107269837+Denismugabe1000@users.noreply.github.com>
2022-06-25 05:43:53 -0700

    Update laptops.php

--- laptops.php
+++ laptops.php
@@ -5,7 +5,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+     <style>
+         h1 { color: azure;
+             margin-inline: 20px;
+         }
+    </style>
     <title>Design Create</title>
 
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
