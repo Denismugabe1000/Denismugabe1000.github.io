d402f022f33f19177a71943efa6bdd9f04eb891f
Denismugabe1000 <107269837+Denismugabe1000@users.noreply.github.com>
2022-06-26 20:48:43 +0300

    Update laptops.php

--- laptops.php
+++ laptops.php
@@ -6,8 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
      <style>
-         body { background color : green: }
-         h1 { color: azure;
+         bod { color: azure;
              margin-inline: 20px;
          }
     </style>
